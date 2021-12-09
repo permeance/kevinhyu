101d359c7bb7df5165bc725b67b2fae33db29949
keviny203 <52251927+keviny203@users.noreply.github.com>
2021-12-09 01:31:09 -0600

    filesystem redirect to projects

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/projects`,
       },
     },
   ],
