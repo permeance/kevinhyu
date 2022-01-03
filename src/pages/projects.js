375630d5e51b449338c5b0b8d22bbe68d4a5df08
keviny203 <52251927+keviny203@users.noreply.github.com>
2022-01-03 14:59:09 -0600

    fix file routing

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../components/layout";
 
 
-import ProjectModals from "/src/components/ProjectModals";
+import ProjectModals from "/src/components/projectModals";
 {/* change to /project/ProjectModals*/}
 
 const ProjectsPage = () => {
